1bf0a251d77b40fbe54762186586406c3932826d
Colin Watson <cjwatson@debian.org>
2019-04-20 11:23:12 +0100

    Remove unnecessary null handling in tests
    "null" is filtered out of the output of "ssh -Q key-plain" in
    sshkey_alg_list, so we don't need to filter it out in regression tests
    as well.

--- regress/cert-userkey.sh
+++ regress/cert-userkey.sh
@@ -7,7 +7,7 @@ rm -f $OBJ/authorized_keys_$USER $OBJ/user_ca_key* $OBJ/cert_user_key*
 cp $OBJ/sshd_proxy $OBJ/sshd_proxy_bak
 cp $OBJ/ssh_proxy $OBJ/ssh_proxy_bak
 
-PLAIN_TYPES=`$SSH -Q key-plain | grep -v null | sed 's/^ssh-dss/ssh-dsa/;s/^ssh-//'`
+PLAIN_TYPES=`$SSH -Q key-plain | sed 's/^ssh-dss/ssh-dsa/;s/^ssh-//'`
 EXTRA_TYPES=""
 
 if echo "$PLAIN_TYPES" | grep '^rsa$' >/dev/null 2>&1 ; then
